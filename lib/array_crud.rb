a23dc0a545741c0c62c3623611ff48909b62c79c
Deanna Kelley <deanna.georges@gmail.com>
2019-09-20 14:53:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,8 +32,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  body_parts = ["brain", "heart", "liver", "feet"]
-  body_parts[0]
+  array = ["I", "am", "really", "learning"]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
